c80230b8690d7f36514ad443178ce5527792c236
Ryan Dajani <ryandajani05@gmail.com>
2023-06-02 11:44:37 -0400

    created UI elements for the activity_book_trip.xml page which include from, to, departureDate and return date as well as a drop down which will include a way for the user to input the number of adult, youth, child and infant passengers. Also made a calendar pop up when the use selects the departure and return date TextViews.

--- app/src/main/java/com/example/tripplanner/bookTrip.java
+++ app/src/main/java/com/example/tripplanner/bookTrip.java
@@ -2,14 +2,82 @@ package com.example.tripplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import android.os.Bundle;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class bookTrip extends MainActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_trip);
 
+        Spinner passengers = findViewById(R.id.selectPassengers);
+        TextView departDate = findViewById(R.id.departDateText);
+        TextView returnDate = findViewById(R.id.returnDateText);
+
+        ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this, R.array.passengerType, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        passengers.setAdapter(adapter);
+
+        departDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar mCurrentDate = Calendar.getInstance();
+                int mYear = mCurrentDate.get(Calendar.YEAR);
+                int mMonth = mCurrentDate.get(Calendar.MONTH);
+                int mDay = mCurrentDate.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog mDatePicker = new DatePickerDialog(bookTrip.this, new DatePickerDialog.OnDateSetListener() {
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(year, monthOfYear, dayOfMonth);
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                        String dateString = dateFormat.format(calendar.getTime());
+                        departDate.setText(dateString);
+                    }
+                }, mYear, mMonth, mDay);
+                mDatePicker.setTitle("Select Departure Date");
+                mDatePicker.show();
+            }
+        });
+
+        returnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar mCurrentDate = Calendar.getInstance();
+                int mYear = mCurrentDate.get(Calendar.YEAR);
+                int mMonth = mCurrentDate.get(Calendar.MONTH);
+                int mDay = mCurrentDate.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog mDatePicker = new DatePickerDialog(bookTrip.this, new DatePickerDialog.OnDateSetListener() {
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.set(year, monthOfYear, dayOfMonth);
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+                        String dateString = dateFormat.format(calendar.getTime());
+                        returnDate.setText(dateString);
+                    }
+                }, mYear, mMonth, mDay);
+                mDatePicker.setTitle("Select Return Date");
+                mDatePicker.show();
+            }
+        });
     }
 }
